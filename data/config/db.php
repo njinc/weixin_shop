cd89b9b2f8cd89995f69c75ef5b5febb184b4c28
njinc <njin.cool@gmail.com>
2014-05-25 08:10:17 +0800

    alter order

--- data/config/db.php
+++ data/config/db.php
@@ -1,9 +1,9 @@
 <?php
-return $arr= array (
-  'DB_HOST' => 'localhost',
-  'DB_NAME' => 'weixin',
-  'DB_USER' => 'weixin',
-  'DB_PWD' => 'NJwx2014',
-  'DB_PORT' => '3306',
-  'DB_PREFIX' => 'weixin_',
+return $arr = array(
+	'DB_HOST'   => '127.0.0.1',
+	'DB_NAME'   => 'weixin',
+	'DB_USER'   => 'weixin',
+	'DB_PWD'    => 'NJwx2014',
+	'DB_PORT'   => '3306',
+	'DB_PREFIX' => 'weixin_',
 );
